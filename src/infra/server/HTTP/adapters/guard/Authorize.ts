4f4d46b8253d4227f95043c00043ce5ac754a02e
José Eduardo Almeida <web2solucoes@gmail.com>
2024-06-03 01:06:55 -0300

    feat: decorator @Authorize guard

---
+++ src/infra/server/HTTP/adapters/guard/Authorize.ts
@@ -0,0 +1,19 @@
+/* eslint-disable func-names */
+/* eslint-disable @typescript-eslint/ban-types */
+export function Authorize(): Function {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    // eslint-disable-next-line no-param-reassign
+    descriptor.value = async function (...args: any[]) {
+      const authorizedUser = await (this as any).authService.authorize(
+        args[0].authorization
+      );
+      (this as any).authService.throwIfUserHasNoAccessToResource(
+        authorizedUser,
+        args[0].schemaOAS
+      );
+      return originalMethod.apply(this, args);
+    };
+    return descriptor;
+  };
+}
